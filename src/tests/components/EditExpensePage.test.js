774df2400f924aa6f8fdf26ada0b335b3ada50dc
frontend <joana@dengun.net>
2019-02-11 16:27:20 +0000

    firebase removing expense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -4,16 +4,16 @@ import toJSON from 'enzyme-to-json';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
 
-let editExpense, removeExpense, history, wrapper;
+let editExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
     editExpense = jest.fn();
-    removeExpense = jest.fn();
+    startRemoveExpense = jest.fn();
     history = { push: jest.fn()};
     wrapper = shallow(
         <EditExpensePage 
             editExpense={editExpense} 
-            removeExpense={removeExpense} 
+            startRemoveExpense={startRemoveExpense} 
             history={history} 
             expense={expenses[2]}
         />
@@ -31,10 +31,10 @@ test('should handle editExpense', () => {
     expect(editExpense).toHaveBeenCalledWith(expenses[2].id, expenses[2]);
 });
 
-test('should handle removeExpense', () => {
+test('should handle startRemoveExpense', () => {
     wrapper.find('button').simulate('click');
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(removeExpense).toHaveBeenCalledWith({ id: expenses[2].id });
+    expect(startRemoveExpense).toHaveBeenCalledWith({ id: expenses[2].id });
 });
 
 
